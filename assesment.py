5f96388477e8a2d41f6f5d0893b9eecfff0767e2
Blair-rayner <24497@burnside.school.nz>
2026-05-27 19:51:04 +1200

    tested database

--- assesment.py
+++ assesment.py
@@ -1,8 +1,9 @@
 import sqlite3 
 
-db = sqlite3.connect('assesment.db')
-
+db = sqlite3.connect('/Users/blairrayner/Downloads/assesment.db')
 cursor = db.cursor()
-
-
+sql = "SELECT * FROM assesment;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
 db.close()
