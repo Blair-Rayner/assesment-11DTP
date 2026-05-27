23013a3133261ab58a18211c519728e91158ef49
Blair-rayner <24497@burnside.school.nz>
2026-05-27 20:10:47 +1200

    Formatting

--- assesment.py
+++ assesment.py
@@ -8,15 +8,19 @@ DATABASE = '/Users/blairrayner/Downloads/assesment.db'
 
 #functions
 def print_all_cyclists():
+    '''print all cyclists'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * FROM assesment;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
+    #loop through results
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<25} {cyclist[3]:<3} {cyclist[4]:<5} {cyclist[5]:<15}")
+    #loop finished
     db.close()
 
 
 
 #main code
-
+print_all_cyclists()
