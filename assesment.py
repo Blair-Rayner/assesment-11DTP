2da61777e28299f6348ca2ad5b053ad472580da8
Blair-rayner <24497@burnside.school.nz>
2026-05-27 19:56:31 +1200

    orginising and adding docstrings

--- assesment.py
+++ assesment.py
@@ -1,9 +1,22 @@
+#docstring - Blair Rayner - assesment database application
+#imports
 import sqlite3 
 
-db = sqlite3.connect('/Users/blairrayner/Downloads/assesment.db')
-cursor = db.cursor()
-sql = "SELECT * FROM assesment;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#constants and variables
+DATABASE = '/Users/blairrayner/Downloads/assesment.db'
+
+
+#functions
+def print_all_cyclists():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.close()
+
+
+
+#main code
+
