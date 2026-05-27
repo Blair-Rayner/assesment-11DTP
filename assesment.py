38d34f1b922cef6968aa861a65897fab41d73ffe
Blair-rayner <24497@burnside.school.nz>
2026-05-27 20:42:39 +1200

    adding a menu

--- assesment.py
+++ assesment.py
@@ -15,12 +15,99 @@ def print_all_cyclists():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
     for cyclist in results:
-        print(f"{cyclist[1]:<22} {cyclist[2]:<25} {cyclist[3]:<3} {cyclist[4]:<5} {cyclist[5]:<15}")
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
     #loop finished
     db.close()
 
+def print_cyclists_by_top_speed():
+    '''print cyclists ordered by top speed'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment ORDER BY top_speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
+    #loop finished
+    db.close()
 
+def print_cyclists_by_average_speed():
+    '''print cyclists ordered by average speed'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment ORDER BY avg_speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
+    #loop finished
+    db.close()
+
+def print_cyclists_by_nationality():
+    '''print cyclists ordered by nationality'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment ORDER BY nationality ASC, top_speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
+    #loop finished
+    db.close()
+
+def print_cyclists_alphabetically():
+    '''print cyclists ordered alphabetically'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment ORDER BY rider_name ASC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
+    #loop finished
+    db.close()
+
+def print_cyclists_by_team():
+    '''print cyclists ordered by team'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM assesment ORDER BY team ASC, top_speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results
+    print(f"{'Name:':<22} {'Team:':<26} {'Top speed:':<12} {'Average speed:':<18} {'Nationality:':<15}")
+    for cyclist in results:
+        print(f"{cyclist[1]:<22} {cyclist[2]:<26} {cyclist[3]:<12} {cyclist[4]:<18} {cyclist[5]:<15}")
+    #loop finished
+    db.close()
 
 #main code
-print_all_cyclists()
+while True:
+    user_input = input("Choose a format: \n1. Print all cyclists \n2. Order cyclists by top speed \n3. Order cyclists by average speed \n4. Order cyclists by nationality \n5. Order cyclists alphabetically  \n6. Order cyclists by team \n7. Exit \n")
+    if user_input == '1':
+        print_all_cyclists()
+    elif user_input == '2':
+        print_cyclists_by_top_speed()
+    elif user_input == '3':
+        print_cyclists_by_average_speed()
+    elif user_input == '4':
+        print_cyclists_by_nationality()
+    elif user_input == '5':
+        print_cyclists_alphabetically()
+    elif user_input == '6':
+        print_cyclists_by_team()
+    elif user_input == '7':
+        print("Exiting program...")
+        break
+    else:
+        print("Invalid input, please try again.")
